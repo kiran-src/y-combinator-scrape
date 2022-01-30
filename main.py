ff87968ef3d488f816567a7b379d27585210a728
Kiran <kiransettyks@gmail.com>
2022-01-30 20:44:14 +0200

    Organised all the data scraped. Created a 'catch' situation when information for certain dataset is not provided.

--- main.py
+++ main.py
@@ -6,26 +6,21 @@ soup = BeautifulSoup(website.text, "html.parser")
 
 print(soup.find(name="a", class_="titlelink"))
 website_info = []
+
 for i in soup.find_all(name="a", class_="titlelink"):
     entry = {
         "Title": i.get_text(),
-        "URL": i.get("href")
+        "URL": i.get("href"),
+        "Score": 0
     }
     website_info.append(entry)
-#for i in soup.find_all(class_="score"):
-#    print(i.get_text())
-#    print("A")
-a = []
-count = 1
-for i in soup.find_all(class_="score"):
-    print(f"{count}. {i}")
-    count+=1
-    a.append(i)
-print(len(a))
-b = []
-count = 1
-for i in soup.select(selector="tr.athing"):
-    print(f"{count}. {i.select('td')[1].select('a[id]')}")
-    count+=1
-    b.append(i)
-print(len(b))
+
+score_entry = soup.find_all(class_="subtext")
+
+for i in range(len(website_info)):
+    if score_entry[i].find(class_='score') is None:
+        website_info[i]["Score"] = 0
+    else:
+        website_info[i]["Score"] = int(score_entry[i].find(name='span', class_='score').get_text().split()[0])
+print(website_info)
+
